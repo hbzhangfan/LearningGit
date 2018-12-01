e188377f122c0155f017cb97a75a11807af83abf
zhangfan <1098730630@qq.com>
2018-12-01 13:39:49 +0800

    UPDATE

--- src/zhangfan/Stream.java
+++ src/zhangfan/Stream.java
@@ -2,6 +2,10 @@ package zhangfan;
 
 public class Stream {
 	
+	public void add(){
+		System.out.println("add 1.0.1");
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("学习GIT1.0.1");
